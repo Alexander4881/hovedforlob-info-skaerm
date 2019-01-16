9297f521a1f83e169e162456323c79c6228d4a64
Tobias Rosenvinge <tobiasRosenvinge@hotmail.com>
2019-01-16 14:10:53 +0100

    Added Bootstrap Global
    Cleaned Index

--- global.php
+++ global.php
@@ -1,9 +1,13 @@
 <?php
 // CSS Global
 $_SITECSS = "./css/site.css";
+$_BOOTSTRAP_CSS = "./css/bootstrap.php";
+$_BOOTSTRAP_CSS_MIN = "./css/bootstrap.min.php";
 
 // JS Global
 $_MAINJS = "./js/main.js";
+$_BOOTSTRAP_JS = "./js/bootstrap.js";
+$_BOOTSTRAP_JS_MIN = "./js/bootstrap.min.js";
 
 // IMAGE Global
 // Type code here..
@@ -11,5 +15,8 @@ $_MAINJS = "./js/main.js";
 // PAGE TITLES
 $_INDEXTITLE = "Test Title";
 
-$_FONTICONS ='https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"';
+// FONTAWESOME
+$_FONTAWESOME_URL ="https://use.fontawesome.com/releases/v5.6.3/css/all.css";
+$_FONTAWESOME_INTEGRITY = "sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/";
+$_FORTAWESOME_CROSSORIGIN = "anonymous";
 ?>
--- index.php
+++ index.php
@@ -6,13 +6,23 @@ include_once "global.php";
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?php $_INDEXTITLE ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php $_INDEXTITLE ?></title>
+
+    <!-- Stylesheets -->
     <link rel="stylesheet" type="text/css" media="screen" href="<?php $_SITECSS ?>" />
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php $_FONTICONS ?>" />
-    <script src="<?php $_MAINJS ?>"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php $_FONTAWESOME_URL ?>" integrity="<?php $_FONTAWESOME_INTEGRITY ?>" crossorigin="<?php $_FORTAWESOME_CROSSORIGIN ?>" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php $_BOOTSTRAP_CSS ?>" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php $_BOOTSTRAP_CSS_MIN ?>" />
 </head>
 <body>
 
 </body>
+
+<!-- Scripts -->
+<script src="<?php $_BOOTSTRAP_JS ?>"></script>
+<script src="<?php $_BOOTSTRAP_JS_MIN ?>"></script>
+<script src="<?php $_MAINJS ?>"></script>
+
 </html>
