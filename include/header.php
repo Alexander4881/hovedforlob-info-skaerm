95a95023d84cd05287c7b8622e53b2e1f664970c
Tobias Rosenvinge <tobiasRosenvinge@hotmail.com>
2019-01-16 16:01:19 +0100

    Done some weird stuff

--- include/header.php
+++ include/header.php
@@ -1,30 +1,28 @@
 <?php
 include_once '../global.php';
 ?>
-<div class="container">
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarToggler">
-      <a class="navbar-brand" href="<?= $_INDEX; ?>"
-        <img src="#" width="30" height="30" class="d-inline-block align-top" alt="<?= $_INDEXTITLE; ?>" />
-        <?= $_INDEXTITLE; ?>
-      </a>
-      <ul class="nav justify-content-center">
-        <li class="nav-item">
-          <a class="nav-link" href="#Link1">Link 1</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#Link2">Link 2</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#Link3">Link 3</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#Link4">Link 4</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-</div>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+  <a class="navbar-brand" href="<?= $_INDEX; ?>">
+    <img src="#" width="30" height="30" class="d-inline-block align-top" alt="" />
+    <?= $_INDEXTITLE; ?>
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarToggler">
+    <ul class="navbar-nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link" href="#Link1">Link 1</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#Link2">Link 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#Link3">Link 3</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#Link4">Link 4</a>
+      </li>
+    </ul>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -4,31 +4,35 @@
 include_once 'global.php';
 ?>
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title><?= $_INDEXTITLE; ?></title>
+  <title><?= $_INDEXTITLE; ?></title>
 
-    <!-- Stylesheets -->
-    <link rel="stylesheet" type="text/css" media="screen" href="<?= $_SITECSS; ?>" />
-    <link rel="stylesheet" type="text/css" media="screen" href="<?= $_FONTAWESOME_URL; ?>" integrity="<?= $_FONTAWESOME_INTEGRITY; ?>" crossorigin="<?= $_FORTAWESOME_CROSSORIGIN; ?>" />
-    <link rel="stylesheet" type="text/css" media="screen" href="<?= $_BOOTSTRAP_CSS; ?>" />
-    <link rel="stylesheet" type="text/css" media="screen" href="<?= $_BOOTSTRAP_CSS_MIN; ?>" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <!-- Stylesheets -->
+  <link rel="stylesheet" type="text/css" media="screen" href="<?= $_SITECSS; ?>" />
+  <link rel="stylesheet" type="text/css" media="screen" href="<?= $_FONTAWESOME_URL; ?>" integrity="<?= $_FONTAWESOME_INTEGRITY; ?>" crossorigin="<?= $_FORTAWESOME_CROSSORIGIN; ?>" />
+  <link rel="stylesheet" type="text/css" media="screen" href="<?= $_BOOTSTRAP_CSS; ?>" />
+  <link rel="stylesheet" type="text/css" media="screen" href="<?= $_BOOTSTRAP_CSS_MIN; ?>" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+  <!-- Scripts -->
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 </head>
 <body>
-<?php
-include_once './include/header.php';
-?>
+  <div class="alert alert-danger" role="alert">
+    Website is in Alpha, be patient.
+  </div>
+  <?php
+  include_once './include/header.php';
+  ?>
+
+  <!-- Scripts -->
+  <script src="<?= $_BOOTSTRAP_JS ?>"></script>
+  <script src="<?= $_BOOTSTRAP_JS_MIN ?>"></script>
+  <script src="<?= $_MAINJS ?>"></script>
 </body>
-
-<!-- Scripts -->
-<script src="<?= $_BOOTSTRAP_JS ?>"></script>
-<script src="<?= $_BOOTSTRAP_JS_MIN ?>"></script>
-<script src="<?= $_MAINJS ?>"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-
 </html>
