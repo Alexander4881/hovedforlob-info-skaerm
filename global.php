a81aa36c151cda602d20bbe43cd89c54cbf6451a
Tobias Rosenvinge <tobiasRosenvinge@hotmail.com>
2019-01-16 13:36:39 +0100

    Global Created
    Global Variables Created, Index updated with globals.

---
+++ global.php
@@ -0,0 +1,13 @@
+<?php
+// CSS Global
+$_SITECSS = "./css/site.css";
+
+// JS Global
+$_MAINJS = "./js/main.js";
+
+// IMAGE Global
+// Type code here..
+
+// PAGE TITLES
+$_INDEXTITLE = "Test Title";
+?>
--- index.php
+++ index.php
@@ -1,25 +1,17 @@
 <!DOCTYPE html>
 <html>
 <?php
-  $testing = "Hello WOrld";
-  $pleaseWork = "!!!!";
+include_once "global.php";
 ?>
-
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title><?php $_INDEXTITLE ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php $_SITECSS ?>" />
+    <script src="<?php $_MAINJS ?>"></script>
 </head>
 <body>
-    <p>
-    <?php
-    echo $testing;
-    echo $pleaseWork;
-    phpinfo();
-    ?>
-    </p>
+
 </body>
 </html>
