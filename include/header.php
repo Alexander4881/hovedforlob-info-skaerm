b80895088a710773b7a4f451e1252a410cb803d4
BusyStick <BusyStick>
2019-01-17 08:37:58 +0100

    New Nav
    implented a new nav bar

--- include/header.php
+++ include/header.php
@@ -1,28 +1,33 @@
 <?php
 include_once '../global.php';
 ?>
-<nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+
+<nav class="navbar navbar-light navbar-expand-md bg-light justify-content-center sticky-top">
   <a class="navbar-brand" href="<?= $_INDEX; ?>">
     <img src="#" width="30" height="30" class="d-inline-block align-top" alt="" />
     <?= $_INDEXTITLE; ?>
   </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarToggler">
-    <ul class="navbar-nav justify-content-center">
-      <li class="nav-item">
-        <a class="nav-link" href="#Link1">Link 1</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#Link2">Link 2</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#Link3">Link 3</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#Link4">Link 4</a>
-      </li>
-    </ul>
-  </div>
+    <button class="navbar-toggler ml-1" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse justify-content-between align-items-center w-100" id="navbarToggler">
+        <ul class="navbar-nav mx-auto text-center">
+          <li class="nav-item">
+            <a class="nav-link" href="#Link1">Link 1</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#Link2">Link 2</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#Link3">Link 3</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#Link4">Link 4</a>
+          </li>
+        </ul>
+        <ul class="nav navbar-nav flex-row justify-content-center flex-nowrap">
+            <li class="nav-item"><a class="nav-link" href=""><i class="fab fa-facebook-f mr-1"></i></a> </li>
+            <li class="nav-item"><a class="nav-link" href=""><i class="fab fa-discord"></i></a> </li>
+        </ul>
+    </div>
 </nav>
