500ed4535721bb0a1774ffee1c7c1c938c661c08
BusyStick <BusyStick>
2019-01-17 10:44:51 +0100

    Create svg.php

---
+++ images/svg.php
@@ -0,0 +1,40 @@
+<svg width="964.0000000000001" height="257" xmlns="http://www.w3.org/2000/svg">
+ <!-- Created with Method Draw - http://github.com/duopixel/Method-Draw/ -->
+ <g>
+  <rect fill="none" id="canvas_background" height="259" width="966" y="-1" x="-1"/>
+  <g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">
+   <rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/>
+  </g>
+ </g>
+ <g id="svg">
+  <g id="Rum-B.16" stroke-width="10" stroke="#d6d6d6" fill="#ff7c7c">
+   <rect id="svg_4" height="244.828488" width="303" y="6.263195" x="6.837376"  />
+   <line stroke-linecap="null" stroke-linejoin="null" id="svg_15" y2="226.060687" x2="212.121278" y1="247.878877" x1="270.303086" stroke-width="5"/>
+  </g>
+  <g id="Rum-B.014" stroke-width="10" stroke="#d6d6d6" fill="#ff7c7c">
+   <rect id="svg_9" height="244.828488" width="303.449394" y="6.263195" x="318.907483" />
+   <line stroke-linecap="null" stroke-linejoin="null" id="svg_18" y2="226.060687" x2="528.484871" y1="247.878877" x1="584.848509" stroke-width="5" />
+   <line transform="rotate(-90 611.2122802734376,196.96966552734372) " stroke-linecap="null" stroke-linejoin="null" id="svg_19" y2="186.060573" x2="639.393927" y1="207.878763" x1="583.030368" stroke-width="5"/>
+  </g>
+  <g id="Rum-B.12" stroke="#d6d6d6" stroke-width="10" fill="#ff7c7c">
+   <rect  id="svg_13" height="122.34487" width="94.827929" y="128.677443" x="629.253417" />
+   <line transform="rotate(-90 714.8484497070314,204.24246215820312) " stroke-linecap="null" stroke-linejoin="null" id="svg_17" y2="193.333299" x2="743.03028" y1="215.151489" x1="686.666721" stroke-width="5" />
+  </g>
+  <g id="Rum-B.10" stroke="#d6d6d6" stroke-width="10" fill="#ff7c7c" >
+   <rect id="svg_5" height="244.828488" width="95" y="6.263197" x="732.702111" />
+   <line transform="rotate(-90 816.6663208007814,204.2424621582031) " stroke-linecap="null" stroke-linejoin="null" id="svg_20" y2="193.333299" x2="844.848451" y1="215.151489" x1="788.484892" stroke-width="5" />
+  </g>
+  <g id="Rum-Gang" stroke="#d6d6d6" stroke-width="10" fill="#ff7c7c">
+   <rect  id="svg_8" height="94.758561" width="94.827929" y="156.263755" x="836.150726" />
+   <line transform="rotate(90 944.8485107421875,206.06068420410165) " stroke-linecap="null" stroke-linejoin="null" id="svg_21" y2="217.878879" x2="973.03019" y1="194.242532" x1="916.66663" stroke-width="5" />
+   <line transform="rotate(0.9128611087799072 909.551330566406,162.57522583007804) " stroke-linecap="null" stroke-linejoin="null" id="svg_22" y2="167.958012" x2="893.105899" y1="157.192387" x1="925.996467" stroke-width="5" />
+  </g>
+  <g id="Rum-Gang" stroke="#d6d6d6" stroke-width="10" fill="#ff7c7c">
+   <rect  id="svg_12" height="115" width="94.827929" y="6.263196" x="629.253431" />
+   <line transform="rotate(180 675.7576904296875,112.00776672363281) " stroke-linecap="null" stroke-linejoin="null" id="svg_28" y2="119.280399" x2="655.757588" y1="104.734946" x1="695.757584" fill-opacity="null" stroke-opacity="null" stroke-width="5" />
+  </g>
+  <g id="Rum-B.08" stroke="#d6d6d6" stroke-width="10" fill="#ff7c7c">
+	<rect  id="svg_6" height="142.586306" width="94.827929" y="6.263198" x="836.150741" />
+  </g>
+ </g>
+</svg>
