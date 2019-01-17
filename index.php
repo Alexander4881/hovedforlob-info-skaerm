26a3ad37b6333a7702a23ea22a2e82175fc13d4c
BusyStick <BusyStick>
2019-01-17 07:50:51 +0100

    CSS Fix
    moved link for css site down so it dosen't gets overwritten by bootstrap css.

--- index.php
+++ index.php
@@ -11,12 +11,11 @@ include_once 'global.php';
   <title><?= $_INDEXTITLE; ?></title>
 
   <!-- Stylesheets -->
-  <link rel="stylesheet" type="text/css" media="screen" href="<?= $_SITECSS; ?>" />
   <link rel="stylesheet" type="text/css" media="screen" href="<?= $_FONTAWESOME_URL; ?>" integrity="<?= $_FONTAWESOME_INTEGRITY; ?>" crossorigin="<?= $_FORTAWESOME_CROSSORIGIN; ?>" />
   <link rel="stylesheet" type="text/css" media="screen" href="<?= $_BOOTSTRAP_CSS; ?>" />
   <link rel="stylesheet" type="text/css" media="screen" href="<?= $_BOOTSTRAP_CSS_MIN; ?>" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-
+  <link rel="stylesheet" type="text/css" media="screen" href="<?= $_SITECSS; ?>" />
   <!-- Scripts -->
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
