8cc428a35895dddda78f3688ef19dafda277944d
Tobias Rosenvinge <tobiasRosenvinge@hotmail.com>
2019-01-18 11:30:37 +0100

    Cleaned Files

--- global.php
+++ global.php
@@ -1,13 +1,22 @@
 <?php
 // CSS Global
 $_SITECSS = './css/site.css';
-$_BOOTSTRAP_CSS = './css/bootstrap.css';
-$_BOOTSTRAP_CSS_MIN = './css/bootstrap.min.css';
 
 // JS Global
-$_MAINJS = "./js/mainJS.js";
+$_MAINJS = "./js/MainJavaScript.js";
+
+// Bootstrap 4.2.1
+$_BOOTSTRAP_CSS = './css/bootstrap.css';
+$_BOOTSTRAP_CSS_MIN = './css/bootstrap.min.css';
 $_BOOTSTRAP_JS = './js/bootstrap.js';
 $_BOOTSTRAP_JS_MIN = './js/bootstrap.min.js';
+
+// URL
+$_BOOTSTRAP_CSS_MIN_URL = 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css';
+$_BOOTSTRAP_CSS_INTEGRITY = 'sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS';
+
+$_BOOTSTRAP_JS_MIN_URL = 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js';
+$_BOOTSTRAP_JS_INTEGRITY = 'sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k';
 
 // IMAGE Global
 // Type code here..
@@ -22,10 +31,15 @@ $_LOCATIONB14 = "../location/lokale-B.14.php";
 // FONTAWESOME
 $_FONTAWESOME_URL ='https://use.fontawesome.com/releases/v5.6.3/css/all.css';
 $_FONTAWESOME_INTEGRITY = 'sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/';
-$_FORTAWESOME_CROSSORIGIN = 'anonymous';
+
+// Crossorigin
+$_CROSSORIGIN = 'anonymous';
 
 // Redirects
 $_INDEX = 'index.php';
+
+// Links
+$_DISCORD = '#Feature-Coming-Soon';
 
 // MySQL Configuration
 $_DBUsername = "root";
