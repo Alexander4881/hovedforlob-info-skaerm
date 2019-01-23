cd47fd2e206db5ad5204801f8c2b4f07e490090e
Tobias Rosenvinge <tobiasRosenvinge@hotmail.com>
2019-01-23 14:21:58 +0100

    Deleted Everything

--- management/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require '../global.php';
-
-// MySQL
-define('DB_USER', '$_DBUsername');
-define('DB_PASSWORD', '$_DBPassword');
-define('DB_HOST', '$_DBHostname');
-define('DB_Catalogue', '$_DBCatalogue');
-?>
